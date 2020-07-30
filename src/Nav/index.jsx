4cd22022d8112855c52ec756dcc5d70512f097dc
Melissa Robertson <melissa.robertson@healthstream.com>
2020-07-30 10:40:54 -0500

    including nav from week 2

---
+++ src/Nav/index.jsx
@@ -0,0 +1,31 @@
+import React,  {useContext} from 'react'
+import { HogwartsContext } from '../HogwartsContext'
+import { NavLink } from 'react-router-dom'
+
+export default function Nav (){
+    const { hogwartsData } = useContext(HogwartsContext);
+  
+    return (
+        <nav className='row space-around'>
+        <ul className='row nav'>
+        <li>
+            <NavLink
+            to='/'
+            exact
+            className='nav-link'
+            activeClassName='active'>
+                Sorting Hat
+            </NavLink>
+        </li>
+        <li>
+            <NavLink
+            to='/common-room'
+            className='nav-link'
+            activeClassName='active'>
+                {hogwartsData.house} Common Room
+            </NavLink>
+        </li>
+        </ul>
+    </nav>
+  )
+}
